ffb691f685b4d148bafc80c7ff89247076a3cc3c
Ivan Shafran <shafran.fun@gmail.com>
2019-02-24 17:38:02 +0300

    Add resource provider class

---
+++ app/src/main/java/com/github/ivanshafran/emotiongame/resource/ResourceProvider.kt
@@ -0,0 +1,17 @@
+package com.github.ivanshafran.emotiongame.resource
+
+import android.content.Context
+import androidx.annotation.DimenRes
+import androidx.annotation.StringRes
+
+class ResourceProvider(private val context: Context) {
+
+    fun getString(@StringRes stringRes: Int, vararg args: Any?): String {
+        return context.getString(stringRes, args)
+    }
+
+    fun getDimen(@DimenRes dimenRes: Int): Int {
+        return context.resources.getDimensionPixelSize(dimenRes)
+    }
+
+}
